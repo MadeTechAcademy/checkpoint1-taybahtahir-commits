374e62a7623aac2ffc9becf29c8deac6b4884cf3
Taybah Tahir <taybah.tahir@madetech.com>
2025-11-24 12:48:04 +0000

    changed duty 6 onwards to be on a seperate line & fixed duplication of duty 12

--- test_themes.py
+++ test_themes.py
@@ -1,4 +1,15 @@
+import unittest
+
 from themes import x2
 def testIt():
     #assert len(x2)>10
     assert True is True
+    
+
+#test
+class TestingDuties(unittest.TestCase):
+    def test_duty_numbers(self):
+        self.assertEqual(len(x2), 13)
+
+if __name__ == "__main__":
+    unittest.main()
